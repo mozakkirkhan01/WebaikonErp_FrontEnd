984b7b44e77c2f98f3422362b2ac24561084ee7d
Md Mozakkir Khan <mozakkirkhan337@gmail.com>
2025-12-31 16:19:53 +0530

    Add ProjectTypeComponent with HTML, CSS, TypeScript, and spec files

---
+++ src/app/project-type/project-type.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-type',
+  templateUrl: './project-type.component.html',
+  styleUrls: ['./project-type.component.css']
+})
+export class ProjectTypeComponent {
+
+}
